a8013c31e471b5cdf881433b17ac2f1457d9584a
jagelvisr <josegagelvis@gmail.com>
2026-06-29 16:21:14 -0400

    integrar vercel conf

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import uvicorn
 from src_python.infrastructure.config import settings
+from src_python.infrastructure.fastapi_app import app
 
 if __name__ == "__main__":
     # Arrancar FastAPI con el servidor Uvicorn optimizando según entorno
     is_development = settings.ENVIRONMENT == "development"
     
     uvicorn.run(
-        "src_python.infrastructure.fastapi_app:app",
+        "app:app",
         host="0.0.0.0",
         port=8000,
         reload=is_development
